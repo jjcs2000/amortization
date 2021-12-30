f6eb30f4dca3907fc04832264552b46470d3f50a
jjcs2000 <joshuatjiang@hotmail.com>
2021-12-29 21:42:20 -0500

    update for 3rd aprty changes

--- main.py
+++ main.py
@@ -1,59 +1,77 @@
+from tkinter import *
 import pandas as pd
+from pandastable import Table, TableModel
 
-mortgage = float(input("Enter your mortgage payment: "))
-#print(mortgage)
-monthOryear = input("Enter month or year: ")
-#print(monthOryear)
-numberOf = int(input("Enter the number of " + monthOryear + ": "))
-#print(numberOf)
-interest = float(input("Enter the interest rate: "))
-#print(interest)
-if monthOryear.lower() == 'month':
-    actual_number = numberOf
-else:
-    actual_number = numberOf * 12
+root = Tk()
 
-monthlyamount = mortgage * interest/1200 * \
-                (1+interest/1200)**actual_number / \
-                ((1+interest/1200)**actual_number - 1)
-print("monthly payment is: ")
-print(round(monthlyamount, 2))
+mortgage = Label(root, text="Enter your mortgage payment: ")
+mortgage.pack()
+mortgageText = Entry(root, width=50)
+mortgageText.pack()
 
-monthOryearDF = input("Enter monthly or yearly amortization schedule: ")
-if monthOryearDF.lower() == 'monthly':
-    arraySize = actual_number
-    isYear = False
-else:
-    arraySize = actual_number
-    isYear = True
+monthOryear = Label(root, text="Enter month or year: ")
+monthOryear.pack()
+monthOryearText = Entry(root, width=50)
+monthOryearText.pack()
 
-paymentList=[]
-interestList=[]
-principalList=[]
-totalInterestList=[]
-balanceList=[]
-for i in range(arraySize):
-    paymentList.append(round(monthlyamount, 2))
-    interestList.append(round(mortgage*interest/1200, 2))
-    principalList.append(round(monthlyamount - interestList[i], 2))
-    if i == 0:
-        totalInterestList.append(interestList[i])
+numberOf = Label(root, text="Enter the number of " + monthOryearText.get() + ": ")
+numberOf.pack()
+numberOfText = Entry(root, width=50)
+numberOfText.pack()
+
+interest = Label(root, text="Enter the interest rate: ")
+interest.pack()
+interestText = Entry(root, width=50)
+interestText.pack()
+
+
+def cal_sum():
+    mortgage = float(mortgageText.get())
+    interest = float(interestText.get())
+    if monthOryearText.get() == 'month':
+        actual_number = int(numberOfText.get())
     else:
-        totalInterestList.append(interestList[i] + totalInterestList[i-1])
-    mortgage=mortgage-principalList[i]
-    balanceList.append(mortgage)
+        actual_number = int(numberOfText.get())
+        actual_number = actual_number * 12
 
-df = pd.DataFrame(columns=['payment', 'principal', 'interest', 'total interest', 'balance'])
-df['payment']=paymentList
-df['interest']=interestList
-df['principal']=principalList
-df['total interest']=totalInterestList
-df['balance']=balanceList
+    monthlyamount = float(mortgageText.get()) * interest/1200 * \
+                    (1+interest/1200)**actual_number / \
+                    ((1+interest/1200)**actual_number - 1)
 
-print(df)
+    monthlyAmount = Label(root, text="Your monthly amount is: " + str(round(monthlyamount, 2)))
+    monthlyAmount.pack()
 
 
+    paymentList=[]
+    interestList=[]
+    principalList=[]
+    totalInterestList=[]
+    balanceList=[]
+    arraySize = actual_number
+
+    for i in range(arraySize):
+        paymentList.append(round(monthlyamount, 2))
+        interestList.append(round(mortgage*interest/1200, 2))
+        principalList.append(round(monthlyamount - interestList[i], 2))
+        if i == 0:
+            totalInterestList.append(round(interestList[i], 2))
+        else:
+            totalInterestList.append(round(interestList[i] + totalInterestList[i-1], 2))
+        mortgage=round(mortgage-principalList[i], 2)
+        balanceList.append(round(mortgage, 2))
+
+    df = pd.DataFrame(columns=['payment', 'principal', 'interest', 'total interest', 'balance'])
+    df['payment']=paymentList
+    df['interest']=interestList
+    df['principal']=principalList
+    df['total interest']=totalInterestList
+    df['balance']=balanceList
+    df.to_csv("output.csv", encoding='utf-8', index=False)
+
+
+myButton = Button(root, text="Calculate Sum", command=cal_sum)
+myButton.pack()
 
 
 
-
+root.mainloop()
